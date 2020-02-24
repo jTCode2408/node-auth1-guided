e67596fb7068dd5af77d80022beb2086365c13b5
jTCode2408 <jasmine.terry91@gmail.com>
2020-02-24 14:10:38 -0500

    end of lecture

--- api/api-router.js
+++ api/api-router.js
@@ -1,12 +1,28 @@
-const router = require('express').Router();
+const bcrypt = require("bcryptjs");
+const router = require("express").Router();
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const restricted = require("../auth/restricted-middleware.js");
 
-router.use('/auth', authRouter);
-router.use('/users', usersRouter);
+router.use("/auth", authRouter);
+router.use("/users", restricted, usersRouter);
 
-router.get('/', (req, res) => {
+router.get("/hash", (req, res) => {
+  // read the Authentication header
+  const authentication = req.headers.authentication;
+
+  // hash the value from that header
+  const hash = bcrypt.hashSync(authentication, 8);
+
+  res.json({ originalValue: authentication, hashedValue: hash });
+
+  // $2a$13$9kEnIDU/c2mGBQq4R6u2eeCcpLBwZOYQXpdnTrezJeBWpJW8D/jDa
+  // $2a$13$anVsq35CZDmj.Fg8LDn6be0WdcJ6EOlLyY0AddqAbuov7EuP/uUIC
+  // $2a$08$zLoclERJ2mulEzJ2q5/sYutJtHDmQrLdZFsrLdeTMCmHwlNBuQCbu
+});
+
+router.get("/", (req, res) => {
   res.json({ api: "It's alive" });
 });
 
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,14 @@
-const router = require('express').Router();
+const bcrypt = require("bcryptjs"); // <<<<<<<<<<<<<<<< npm i bcryptjs
+const router = require("express").Router();
 
-const Users = require('../users/users-model.js');
+const Users = require("../users/users-model.js");
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 8); // << 1
+
+  user.password = hash; // <<<<<<<<<<<<<<<<<<<<<<<<<<<< 2
 
   Users.add(user)
     .then(saved => {
@@ -14,16 +19,16 @@ router.post('/register', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
